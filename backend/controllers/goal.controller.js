d03bf23f265b585651aca81f657a3056bc2c1b5a
Tomás Opazo <tomas.opazoasd@gmail.com>
2022-12-27 19:27:18 -0300

    controller updated to work with db

--- backend/controllers/goal.controller.js
+++ backend/controllers/goal.controller.js
@@ -1,17 +1,69 @@
+const Goal = require("../models/goal.model");
+
 const getAllGoals = async (req, res) => {
-  res.send("get all goals");
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 };
 
 const createGoal = async (req, res) => {
-  res.send("create goal");
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    return res
+      .status(400)
+      .json({ msg: "por favor ingrese título y descripción" });
+  }
+
+  const newGoal = new Goal({
+    title: title,
+    description: description,
+  });
+
+  const goal = await newGoal.save();
+  res.status(201).json(goal);
 };
 
 const updateGoal = async (req, res) => {
-  res.send("update goal");
+  const { title, description } = req.body;
+  const { id } = req.params;
+
+  if (!title || !description) {
+    return res
+      .status(400)
+      .json({ msg: "por favor ingrese título y descripción" });
+  }
+
+  const goal = await Goal.findByIdAndUpdate(id);
+
+  if (!goal) {
+    return res.status(404).json({ msg: "no se encontró el objetivo" });
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(
+    id,
+    {
+      title,
+      description,
+    },
+    { new: true }
+  );
+
+  res.status(200).json(updatedGoal);
 };
 
 const deleteGoal = async (req, res) => {
-  res.send("delete goal");
+  const { id } = req.params;
+  const goal = await Goal.findById(id);
+
+  if (!goal) {
+    return res.status(404).json({ msg: "no se encontró el objetivo" });
+  }
+
+  if (!id) {
+    return res.status(400).json({ msg: "ingrese id del objetivo" });
+  }
+
+  res.status(200).json({ msg: "objetivo eliminado" });
 };
 
 module.exports = {
