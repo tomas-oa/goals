3e5b9be673fcb5ead31f9a54351834f660c497be
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-01-02 18:24:23 -0300

    auth slice + reset reducer

---
+++ client/src/features/auth/auth.slice.js
@@ -0,0 +1,24 @@
+// login - register - logout
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = {
+  user: user ? user : null,
+  error: false,
+  success: false,
+  loading: false,
+  message: ""
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.error = false;
+      state.success = false;
+      state.loading = false;
+      state.message = "";
+    }
+  }
