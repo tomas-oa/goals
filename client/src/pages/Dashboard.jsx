f6d3910b583c72504d21e04c51ae8e33202eda35
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-01-03 20:27:58 -0300

    persistance of goals in localstorage

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,17 +1,38 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goal.slice";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const { user } = useSelector((state) => state.auth);
+  const { goals, loading, error, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
+    if (error) {
+      console.log(message);
+    }
+
     if (!user) {
       navigate("/login");
     }
-  }, [user, navigate]);
+
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, error, message, dispatch]);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <>
